e1115cfa97bd0e26abc00550e10372915a44e07f
sai <saigolang@gmail.com>
2019-10-15 15:38:56 -0500

    refactored the code

--- cmd/AttYamlToJson/main.go
+++ cmd/AttYamlToJson/main.go
@@ -2,24 +2,39 @@ package main
 
 import (
 	"AttYamlToJson/pkg/converter"
+	"AttYamlToJson/pkg/models"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
+	"net/http"
 )
 
 func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	// todo configure logging here
+	router.HandleFunc("/employees", employees).Methods(http.MethodGet)
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
 
+func employees(w http.ResponseWriter, r *http.Request) {
 	rawData, err := ioutil.ReadFile("employee.yaml")
 	if err != nil {
 		fmt.Println("error in converting yaml file to bytes is ", err.Error())
 	}
-
 	// convert yaml to json
 	response := converter.YamlToJson(rawData)
-
-	log.Println("response is ", response)
+	// writing final response to server
+	writeResponseToServer(w, response)
 }
-
-func UnMarhsalYaml() {
-
+func writeResponseToServer(w http.ResponseWriter, finalResponse models.Employees) {
+	respBodyBytes := new(bytes.Buffer)
+	err := json.NewEncoder(respBodyBytes).Encode(&finalResponse)
+	if err != nil {
+		log.Println("error in marshalling the response")
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(respBodyBytes.Bytes())
 }
--- pkg/models/response.go
+++ pkg/models/response.go
@@ -1,17 +1,17 @@
 package models
 
 type Employees struct {
-	Employees    []Employee `json:employee`
-	ErrorMessage ErrorLog   `json:ErrorMessage`
+	Employees    []Employee `json:"employee,omitempty"`
+	ErrorMessage ErrorLog   `json:"ErrorMessage,omitempty"`
 }
 
 type Employee struct {
-	FirstName string `json:firstName`
-	LastName  string `json:lastName`
-	ID        string `json:id`
+	FirstName string `json:"firstName,omitempty"`
+	LastName  string `json:"lastName,omitempty"`
+	ID        string `json:"id,omitempty"`
 }
 
 type ErrorLog struct {
-	StatusCode string `json:statusCode`
-	RootCause  string `json:rootCause`
+	StatusCode string `json:"statusCode,omitempty"`
+	RootCause  string `json:"rootCause,omitempty"`
 }
